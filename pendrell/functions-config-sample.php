923a646b6e50f862de7d310eeb57cdc50d8c05f2
Alexander Synaptic <alexander@synapticism.com>
2014-02-24 21:24:54 +0800

    Refactoring nightmarish CSS spaghetti.

--- pendrell/functions-config-sample.php
+++ pendrell/functions-config-sample.php
@@ -16,7 +16,7 @@ define( 'PENDRELL_AUTHOR_BOX', true );
 define( 'PENDRELL_GOOGLE_ANALYTICS_CODE', false );
 
 // Google web fonts to load; false will load Open Sans
-define( 'PENDRELL_GOOGLE_FONTS', 'Varela+Round:400|Lato:100,400italic,400,700'); // |Open+Sans:400italic,700italic,400,700');
+define( 'PENDRELL_GOOGLE_FONTS', 'Varela+Round:400|Lato:400italic,400,700'); // |Open+Sans:400italic,700italic,400,700');
 
 // Shadow categories: add category numbers to this string in the format '-1,-2' to hide them from the front page
 define( 'PENDRELL_SHADOW_CATS', false );
