4c1b771d236af32b7b169c4a4bb63cdd1a3631b3
Alexander Synaptic <alexander@synapticism.com>
2014-04-07 02:41:54 +0800

    Minor changes, full white background, breadcrumbs navigation.

--- pendrell/functions/archive.php
+++ pendrell/functions/archive.php
@@ -1,4 +1,4 @@
-<?php // === ARCHIVES === //
+<?php // ==== ARCHIVES ==== //
 
 // Generates all-purpose archive titles
 function pendrell_archive_title() {
@@ -9,15 +9,13 @@ function pendrell_archive_title() {
   } elseif ( is_year() ) {
     $title = sprintf( __( 'Yearly archives: %s', 'pendrell' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'pendrell' ) ) . '</span>' );
   } elseif ( is_author() ) {
-    //$title = sprintf( __( 'Posts by %s', 'pendrell' ), '<span>' . get_the_author_meta( 'display_name', get_query_var( 'author' ) ) . '</span>' );
-    //$title = sprintf( __( 'Posts by %s', 'pendrell' ), '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( "ID" ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
-    $title = sprintf( __( 'Posts by %s', 'pendrell' ), '<span class="vcard fn n">' . get_the_author() . '</span>' );
+    $title = sprintf( __( 'Posts by %s', 'pendrell' ), '<span>' . get_the_author() . '</span>' );
   } elseif ( is_category() ) {
-    $title = sprintf( __( '%s category archives', 'pendrell' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+    $title = sprintf( __( '%s archives', 'pendrell' ), '<span>' . single_cat_title( '', false ) . '</span>' );
   } elseif ( is_post_type_archive() ) {
     $title = sprintf( __( '%s archives', 'pendrell' ), '<span>' . post_type_archive_title( '', false ) . '</span>' );
   } elseif ( is_tag() ) {
-    $title = sprintf( __( 'Posts tagged <mark>%s</mark>', 'pendrell' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+    $title = sprintf( __( '%s archives', 'pendrell' ), '<span>' . single_tag_title( '', false ) . '</span>' );
   } elseif ( is_tax() ) {
     if ( is_tax( 'post_format') && get_post_format() === 'quote' ) {
       $title = sprintf( __( '%s archives', 'pendrell' ), '<span>' . __( 'Quotation', 'pendrell' ) . '</span>' );
@@ -41,16 +39,20 @@ function pendrell_archive_description() {
     // Only output archive descriptions for categories, tags, taxonomies, and authors
     if ( is_category() || is_tag() || is_tax() ) {
 
+      ?><div class="archive-content entry-content"><?php
+
+      do_action( 'pendrell_archive_term_before' );
+
       // Check to see if we have a description for this category, tag, or taxonomy
       $description = term_description();
 
       // Got something?
-      if ( !empty( $description ) ) {
-        ?><div class="archive-content entry-content">
-          <?php echo $description;
-          do_action( 'pendrell_archive_description_after' ); ?>
-        </div><?php
-      }
+      if ( !empty( $description ) )
+        echo $description;
+
+      do_action( 'pendrell_archive_term_after' );
+
+      ?></div><?php
 
     } elseif ( is_author() ) {
       if ( get_the_author_meta( 'description' ) ) {
