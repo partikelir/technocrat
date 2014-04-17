4cbcbfde2bbe7ad96622107e42b3fb889342f51b
Alexander Synaptic <alexander@synapticism.com>
2014-04-17 14:44:43 +0800

    Conditional archive descriptions.

--- pendrell/lib/archive.php
+++ pendrell/lib/archive.php
@@ -11,11 +11,11 @@ function pendrell_archive_title() {
   } elseif ( is_author() ) {
     $title = sprintf( __( 'Posts by %s', 'pendrell' ), pendrell_name_wrapper( get_the_author() ) );
   } elseif ( is_category() ) {
-    $title = sprintf( __( '%s archives', 'pendrell' ), pendrell_name_wrapper( single_cat_title( '', false ) ) );
+    $title = sprintf( __( '%s', 'pendrell' ), pendrell_name_wrapper( single_cat_title( '', false ) ) );
   } elseif ( is_post_type_archive() ) {
-    $title = sprintf( __( '%s archives', 'pendrell' ), pendrell_name_wrapper( post_type_archive_title( '', false ) ) );
+    $title = sprintf( __( '%s', 'pendrell' ), pendrell_name_wrapper( post_type_archive_title( '', false ) ) );
   } elseif ( is_tag() ) {
-    $title = sprintf( __( '%s archives', 'pendrell' ), pendrell_name_wrapper( single_tag_title( '', false ) ) );
+    $title = sprintf( __( '%s', 'pendrell' ), pendrell_name_wrapper( single_tag_title( '', false ) ) );
   } elseif ( is_tax() ) {
     if ( is_tax( 'post_format' ) && get_post_format() === 'quote' ) {
       $title = sprintf( __( '%s archives', 'pendrell' ), pendrell_name_wrapper( __( 'Quotation', 'pendrell' ) ) );
@@ -39,21 +39,24 @@ function pendrell_archive_description() {
     // Only output archive descriptions for categories, tags, taxonomies, and authors
     if ( is_category() || is_tag() || is_tax() ) {
 
-      ?><div class="archive-content"><?php // @TODO: make this conditional; it messes up the visual flow
+      $content = '';
 
-      do_action( 'pendrell_archive_term_before' );
+      $content = apply_filters( 'pendrell_archive_term_before', $content );
 
       // Check to see if we have a description for this category, tag, or taxonomy
       $description = term_description();
 
       // Got something?
       if ( !empty( $description ) ) {
-        ?><div class="archive-description"><?php echo do_shortcode( $description ); ?></div><?php
+        $content .= '<div class="archive-description">' . do_shortcode( $description ) . '</div>';
       }
 
-      do_action( 'pendrell_archive_term_after' );
+      $content = apply_filters( 'pendrell_archive_term_after', $content );
 
-      ?></div><?php
+      // Conditional output
+      if ( !empty( $content ) ) {
+        ?><div class="archive-content"><?php echo $content; ?></div><?php
+      }
 
     } elseif ( is_author() ) {
       if ( get_the_author_meta( 'description' ) ) {
