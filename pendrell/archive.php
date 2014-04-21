e58719629aed95b7616ae5d24d4b0c947614ed75
Alexander Synaptic <alexander@synapticism.com>
2014-04-21 19:17:59 +0800

    Empty archives now show descriptions.

--- pendrell/archive.php
+++ pendrell/archive.php
@@ -9,19 +9,19 @@ get_header(); ?>
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php if ( have_posts() ) { ?>
 			<header class="archive-header">
 				<h1 class="archive-title"><?php pendrell_archive_title(); ?></h1>
 				<?php pendrell_archive_description(); ?>
 			</header><!-- .archive-header -->
-			<?php pendrell_content_nav( 'nav-above' );
-			while ( have_posts() ) : the_post();
-				get_template_part( 'content', pendrell_content_template() );
-			endwhile;
-			pendrell_content_nav( 'nav-below' );
-		} else {
-			get_template_part( 'content', 'none' );
-		} ?>
+			<?php if ( have_posts() ) { ?>
+				<?php pendrell_content_nav( 'nav-above' );
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', pendrell_content_template() );
+				endwhile;
+				pendrell_content_nav( 'nav-below' );
+			} else {
+				get_template_part( 'content', 'none' );
+			} ?>
 		</main><!-- #content -->
 	</section><!-- #primary -->
 
--- pendrell/content-none.php
+++ pendrell/content-none.php
@@ -16,6 +16,9 @@
 			<?php if ( is_search() ) { ?>
 				<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again:', 'pendrell' ); ?></p>
 				<?php $search_term = esc_url( $_SERVER['REQUEST_URI'] ); pendrell_search_form( $search_term ); ?>
+			<?php } elseif ( is_archive() ) { ?>
+				<p><?php _e( 'Sorry, there is no content here yet. Try searching for something else:', 'pendrell' ); ?></p>
+				<?php pendrell_search_form(); ?>
 			<?php } else { ?>
 				<p><?php _e( 'What are you looking for? Try searching for it:', 'pendrell' ); ?></p>
 				<?php pendrell_search_form(); ?>
