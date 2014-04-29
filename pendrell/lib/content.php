c4ffbe60633ce86c58aa44a2db0a0f227f066cd0
Alexander Synaptic <alexander@synapticism.com>
2014-04-29 17:38:03 +0800

    Spacing.

--- pendrell/lib/content.php
+++ pendrell/lib/content.php
@@ -94,6 +94,6 @@ if ( !function_exists( 'pendrell_comments_template' ) ) : function pendrell_comm
 
   // If comments are open or we have at least one comment, load up the comment template; via _s
   if ( comments_open() || get_comments_number() != '0' ) {
-    comments_template('', true);
+    comments_template( '', true );
   }
 } endif;
