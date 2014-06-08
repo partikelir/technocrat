d95d9946bed8da74586af567b13847f02621235b
Alexander Synaptic <alexander@synapticism.com>
2014-06-09 03:29:37 +0800

    Link post format, :before/:after consistency

--- pendrell/content-link.php
+++ pendrell/content-link.php
@@ -13,7 +13,7 @@ if ( empty( $link_url ) )
 			<h1 class="entry-title">
 				<?php pendrell_content_title(); ?>
 			</h1>
-			<footer><a href="<?php echo $link_url; ?>" rel="bookmark"><?php echo $link_url; ?></a></footer>
+			<footer class="entry-link"><a href="<?php echo $link_url; ?>" rel="bookmark"><?php echo $link_url; ?></a></footer>
 		</header><!-- .entry-header -->
 		<div class="entry-content">
 			<?php the_content(); ?>
