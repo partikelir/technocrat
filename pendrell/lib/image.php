3265241e14aae1e52c2c5e8eef7b163c4d6aaf79
Alexander Synaptic <alexander@synapticism.com>
2014-07-05 02:11:31 +0800

    Image display improvements

--- pendrell/lib/image.php
+++ pendrell/lib/image.php
@@ -43,47 +43,34 @@ add_filter( 'the_content', 'pendrell_image_wrapper' );
 // Thin wrapper for ubik_image_markup with graceful fallback
 if ( !function_exists( 'pendrell_image_markup' ) ) : function pendrell_image_markup( $html, $id, $caption, $title = '', $align = 'none', $url = '', $size = 'medium' ) {
 
-    // If Ubik is installed...
-    if ( function_exists( 'ubik_image_markup' ) ) {
-      $content = ubik_image_markup( $html, $id, $caption, $title = '', $align = 'none', $url = '', $size );
+  // If Ubik is installed...
+  if ( function_exists( 'ubik_image_markup' ) ) {
+    $content = ubik_image_markup( $html, $id, $caption, $title, $align, $url, $size );
 
-    // This stuff works but Ubik does things in a slightly more refined way
+  // This stuff works but Ubik does things in a slightly more refined way
+  } else {
+
+    // Feeds won't validate with fancy HTML5 tags; let's keep things simply
+    if ( is_feed() ) {
+      $content = $html;
+      if ( !empty( $caption ) )
+        $content .= '<br/><small>' . $caption . '</small> ';
+
+    // Produce some simple HTML5 markup for images and captions
     } else {
-
-      // Feeds won't validate with fancy HTML5 tags; let's keep things simply
-      if ( is_feed() ) {
-        $content = $html;
-        if ( !empty( $caption ) )
-          $content .= '<br/><small>' . $caption . '</small> ';
-
-      // Produce some simple HTML5 markup for images and captions
-      } else {
-        $content = '<figure id="' . $id . '" class="wp-caption wp-caption-' . $id . ' ' . esc_attr( $align ) . '" itemscope itemtype="http://schema.org/ImageObject">' . $html;
-        if ( !empty( $caption ) )
-          $content .= '<figcaption id="figcaption-' . $id . '" class="wp-caption-text" itemprop="caption">' . $caption . '</figcaption>';
-        $content .= '</figure>' . "\n";
-      }
+      $content = '<figure id="' . $id . '" class="wp-caption wp-caption-' . $id . ' ' . esc_attr( $align ) . '" itemscope itemtype="http://schema.org/ImageObject">' . $html;
+      if ( !empty( $caption ) )
+        $content .= '<figcaption id="figcaption-' . $id . '" class="wp-caption-text" itemprop="caption">' . $caption . '</figcaption>';
+      $content .= '</figure>' . "\n";
     }
+  }
   return $content;
 } endif;
 
 
 
-// Add featured images to the feed
-function pendrell_image_feed( $content ) {
-
-  // We should only need this on image format posts
-  if ( has_post_format( 'image' ) && has_post_thumbnail() )
-    $content = pendrell_image_wrapper() . $content;
-
-  return $content;
-}
-add_filter( 'the_content_feed', 'pendrell_image_feed' );
-
-
-
 // Image shortcode fallback (in case Ubik is not active)
-function pendrell_image_shortcode( $atts, $caption = null ) {
+if ( !function_exists( 'pendrell_image_shortcode' ) ) : function pendrell_image_shortcode( $atts, $caption = null ) {
   extract( shortcode_atts( array(
     'id'            => '',
     'title'         => '',
@@ -100,6 +87,14 @@ function pendrell_image_shortcode( $atts, $caption = null ) {
   $html = get_image_tag( $id, $alt, $title, $align, $size );
 
   return apply_filters( 'pendrell_image_shortcode', pendrell_image_markup( $html, $id, $caption, $title, $align, $url, $size, $alt ) );
-}
+} endif;
 if ( !function_exists( 'ubik_image_shortcode' ) )
   add_shortcode( 'image', 'pendrell_image_shortcode' );
+
+
+
+// Wrapper for ubik_thumbnail_id()
+if ( !function_exists( 'pendrell_thumbnail_id' ) ) : function pendrell_thumbnail_id( $post_id = null, $fallback_id = null ) {
+  if ( function_exists( 'ubik_thumbnail_id' ) )
+    return ubik_thumbnail_id( $post_id, $fallback_id );
+} endif;
