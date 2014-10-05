98fc4c5dd0c2f410513b8ac4caf5940b817bbeac
Alexander Synaptic <alexander@synapticism.com>
2014-10-05 00:40:30 -0400

    Views button fix using jQuery

--- src/js/core.js
+++ src/js/core.js
@@ -1,3 +1,12 @@
 // ==== CORE ==== //
 
 // Anything entered here will end up at the top of pendrell-core.js
+
+// For compatibility with Ajaxinate be sure to bind events to a DOM element that isn't ever replaced (e.g. `body`)
+
+;(function($){
+  $('body').on('click', '.view-options button', function(){
+    $(this).toggleClass('view-options-on');
+    $('.button-dropdown').toggle(200, 'swing');
+  });
+}(jQuery));
