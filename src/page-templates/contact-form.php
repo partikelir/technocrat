da4443b7bed4cb7df5e76a9c76f60790407c762f
Alexander Synaptic <alexander@synapticism.com>
2014-11-16 22:24:04 +0800

    Updating page templates to new markup

--- src/page-templates/contact-form.php
+++ src/page-templates/contact-form.php
@@ -10,49 +10,51 @@
  */
 
 get_header(); ?>
-
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
-          <header class="entry-header">
-            <?php pendrell_entry_title(); ?>
-          </header>
-          <div class="entry-content">
-            <?php the_content(); ?>
-          </div>
-          <div>
-            <h3><a name="contact-form"></a><?php _e( 'Contact form', 'pendrell' ); ?></h3>
-            <form id="response-form" class="contact-form" method="post" action="" enctype="application/x-www-form-urlencoded">
-              <p><?php _e( 'Required fields are marked <span class="required">*</span>', 'pendrell' ); ?></p>
-
-              <label for="from"><?php _e( 'Name', 'pendrell' ); ?> <span class="required">*</span></label>
-              <input id="from" name="from" type="text" placeholder="<?php _e( 'Your name', 'pendrell' ); ?>" value="" />
-
-              <label for="email"><?php _e( 'Email', 'pendrell' ); ?> <span class="required">*</span></label>
-              <input id="email" name="email" type="text" placeholder="<?php esc_attr_e( 'your@email.com', 'pendrell' ); ?>" value="" />
-
-              <label for="subject"><?php _e( 'Subject', 'pendrell' ); ?></label>
-              <input id="subject" name="subject" type="text" placeholder="<?php _e( 'What is this about?', 'pendrell' ); ?>" value="" />
-
-              <label for="text"><?php _e( 'Message', 'pendrell' ); ?></label>
-              <textarea id="message" name="message" rows="5" placeholder="<?php esc_attr_e( 'Your message&#x0085;', 'pendrell' ); ?>"></textarea>
-
-              <input type="checkbox" id="cc" name="cc" value="1" />
-              <label for="cc" style="display: inline;"><?php _e( 'Send a copy to yourself', 'pendrell' ); ?></label>
-
-              <div style="display: none;">
-                <label for="text"><?php _e( 'Spam protection; don\'t fill this', 'pendrell' ); ?></label>
-                <input name="hades" type="text" />
+  <div id="wrap-content">
+    <div id="content" class="site-content<?php pendrell_content_class(); ?>">
+    	<section id="primary" class="content-area">
+    		<main id="main" class="site-main" role="main">
+    			<?php while ( have_posts() ) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
+              <header class="entry-header">
+                <?php pendrell_entry_title(); ?>
+              </header>
+              <div class="entry-content">
+                <?php the_content(); ?>
               </div>
+              <div>
+                <h3><a name="contact-form"></a><?php _e( 'Contact form', 'pendrell' ); ?></h3>
+                <form id="response-form" class="contact-form" method="post" action="" enctype="application/x-www-form-urlencoded">
+                  <p><?php _e( 'Required fields are marked <span class="required">*</span>', 'pendrell' ); ?></p>
 
-              <?php wp_nonce_field( 'contact_form', '_contact_form_nonce' ); ?>
-              <input id="submit" name="submit" type="submit" value="<?php esc_attr_e( 'Send message', 'pendrell' ); ?>" />
-            </form>
-          </div>
-        </article>
-			<?php endwhile; // end of the loop. ?>
-		</main>
-	</section>
+                  <label for="from"><?php _e( 'Name', 'pendrell' ); ?> <span class="required">*</span></label>
+                  <input id="from" name="from" type="text" placeholder="<?php _e( 'Your name', 'pendrell' ); ?>" value="" />
 
+                  <label for="email"><?php _e( 'Email', 'pendrell' ); ?> <span class="required">*</span></label>
+                  <input id="email" name="email" type="text" placeholder="<?php esc_attr_e( 'your@email.com', 'pendrell' ); ?>" value="" />
+
+                  <label for="subject"><?php _e( 'Subject', 'pendrell' ); ?></label>
+                  <input id="subject" name="subject" type="text" placeholder="<?php _e( 'What is this about?', 'pendrell' ); ?>" value="" />
+
+                  <label for="text"><?php _e( 'Message', 'pendrell' ); ?></label>
+                  <textarea id="message" name="message" rows="5" placeholder="<?php esc_attr_e( 'Your message&#x0085;', 'pendrell' ); ?>"></textarea>
+
+                  <input type="checkbox" id="cc" name="cc" value="1" />
+                  <label for="cc" style="display: inline;"><?php _e( 'Send a copy to yourself', 'pendrell' ); ?></label>
+
+                  <div style="display: none;">
+                    <label for="text"><?php _e( 'Spam protection; don\'t fill this', 'pendrell' ); ?></label>
+                    <input name="hades" type="text" />
+                  </div>
+
+                  <?php wp_nonce_field( 'contact_form', '_contact_form_nonce' ); ?>
+                  <input id="submit" name="submit" type="submit" value="<?php esc_attr_e( 'Send message', 'pendrell' ); ?>" />
+                </form>
+              </div>
+            </article>
+    			<?php endwhile; // end of the loop. ?>
+    		</main>
+    	</section>
+    </div>
+  </div>
 <?php get_footer(); ?>
--- src/page-templates/full-width.php
+++ src/page-templates/full-width.php
@@ -10,14 +10,16 @@
  */
 
 get_header(); ?>
-
-  <section id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-      <?php while ( have_posts() ) : the_post();
-        pendrell_template_part();
-      endwhile; ?>
-    </main>
-    <?php pendrell_nav_page( 'nav-below' ); ?>
-  </section>
-
+  <div id="wrap-content">
+    <div id="content" class="site-content<?php pendrell_content_class(); ?>">
+      <section id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+          <?php while ( have_posts() ) : the_post();
+            pendrell_template_part();
+          endwhile; ?>
+        </main>
+        <?php pendrell_nav_page( 'nav-below' ); ?>
+      </section>
+    </div>
+  </div>
 <?php get_footer(); ?>
