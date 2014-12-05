5382309abe8172e9c9f04c460222e42023c327a9
Alexander Synaptic <alexander@synapticism.com>
2014-12-05 16:10:44 +0800

    Slowly getting somewhere

--- dist/pendrell/modules/picturefill.php
+++ dist/pendrell/modules/picturefill.php
@@ -1,16 +1,31 @@
 <?php // ==== PICTUREFILL ==== //
 
-// Setup a few media queries for the `sizes` attribute; must be an array
+// Setup a few media queries for the `sizes` attribute; must be an array even if there is only one value
 if ( !function_exists( 'pendrell_sizes_media_queries' ) ) : function pendrell_sizes_media_queries( $queries ) {
 
-  global $main_width;
+  global $content_width, $main_width;
 
-  // Breakpoints replicated from `src/scss/_base_config.scss` without pixel values
-  $mq_small = $main_width/1.2;
-  $mq_medium = $main_width + ( PENDRELL_BASELINE * 3);
-  $mq_large = $main_width + ( PENDRELL_BASELINE * 8);
+  // Flush the array? The order of the `sizes` attributes matters but this prevents us from hooking the same filter elsewhere
+  $queries = array();
 
-  //$queries[] = '(min-width: ' . $mq_large . 'px) 100%';
+  // Breakpoints replicated from `src/scss/_base_config.scss`
+  $full = $content_width + ( PENDRELL_BASELINE * 3) . 'px';
+  $large = $main_width + ( PENDRELL_BASELINE * 8) . 'px';
+  $medium = $main_width + ( PENDRELL_BASELINE * 3) . 'px';
+  $small = $main_width/1.2 . 'px';
+
+  // Full-width is a special case and requires its own media queries
+  if ( !pendrell_is_full_width() ) {
+    // Above the full breakpoint full-width images will max out at $content_width
+    $queries[] = '(min-width: ' . $full . ') ' . $content_width . 'px';
+  } else {
+    // Above the medium breakpoint most images will max out at $main_width
+    $queries[] = '(min-width: ' . $medium . ') ' . $main_width . 'px';
+  }
+
+  // Note: below the previously specified breakpoints most images will expand to fill the window (minus margins)
+  // For this reason no further media queries are specified; the default "100vw" is good enough to handle these cases
+  // @TODO: handle half/third/quarter width images and similar layouts
 
   return $queries;
 
@@ -19,15 +34,8 @@ add_filter( 'ubik_imagery_sizes_media_queries', 'pendrell_sizes_media_queries' )
 
 
 
-// Return $content_width as the default `sizes` fallback
+// An example function; there's no need to filter the default "100vw" with this theme
 if ( !function_exists( 'pendrell_sizes_default' ) ) : function pendrell_sizes_default( $default ) {
-
-  global $content_width;
-
-  if ( isset( $content_width ) )
-    $default = $content_width . 'px';
-
   return $default;
-
 } endif;
-add_filter( 'ubik_imagery_sizes_default', 'pendrell_sizes_default' );
+//add_filter( 'ubik_imagery_sizes_default', 'pendrell_sizes_default' );
