1d449423f1b7c713a281f6987f7ac6ba23e91280
Alexander Synaptic <alexander@synapticism.com>
2014-12-06 02:28:24 +0800

    Another round of Picturefill experiments

--- src/modules/picturefill.php
+++ src/modules/picturefill.php
@@ -5,41 +5,53 @@ if ( !function_exists( 'pendrell_sizes_media_queries' ) ) : function pendrell_si
 
   global $content_width, $main_width;
 
+  // Media queries for this theme should be fairly straight-forward as the layout is fairly simple
+  // Either content is displayed with a sidebar or it's all displayed in a single column
+  // The main thing that varies is the margins (which get progressively smaller as the viewport width decreases)
+
   // Breakpoints replicated from `src/scss/_base_config.scss`
-  $full = $content_width + ( PENDRELL_BASELINE * 3) . 'px';
-  $large = $main_width + ( PENDRELL_BASELINE * 8) . 'px';
-  $medium = $main_width + ( PENDRELL_BASELINE * 3) . 'px';
-  $small = $main_width/1.2 . 'px';
+  $full   = $content_width + ( PENDRELL_BASELINE * 3);
+  $large  = $main_width + ( PENDRELL_BASELINE * 8);
+  $medium = $main_width + ( PENDRELL_BASELINE * 3);
+  $small  = round( $main_width/1.2 );
 
   // Note: the *order* of media queries is important; browsers pick the first match!
 
   // Full-width is a special case and requires its own media queries
   if ( pendrell_is_full_width() ) {
 
-    // Above the full breakpoint full-width images will max out at $content_width
-    if ( empty( $width ) || $width >= $content_width ) {
-      $queries[] = '(min-width: ' . $full . ') ' . $content_width . 'px';
+    if ( !empty( $width ) ) {
+
+      // Approach: start with the largest media query and move down as long as the image width is less than one breakpoint and more than the next
+      if ( $width > $medium ) {
+        $queries[] = '(min-width: ' . ( $width + ( PENDRELL_BASELINE * 3 ) ) . 'px) ' . $width . 'px';
+      } elseif ( $width <= $medium && $width > $small ) {
+        $queries[] = '(min-width: ' . ( $width + ( PENDRELL_BASELINE * 2 ) ) . 'px) ' . $width . 'px';
+      } else {
+        $queries[] = '(min-width: ' . ( $width + PENDRELL_BASELINE ) . 'px) ' . $width . 'px';
+      }
     } else {
-      // Approach: start with the largest media query and move down as long as the image width is less than the breakpoint
-      $queries[] = '(min-width: ' . $full . ') ' . $width . 'px';
-      if ( $width <= $large )
-        $queries[] = '(min-width: ' . $large . ') ' . $width . 'px';
-      if ( $width <= $medium )
-        $queries[] = '(min-width: ' . $medium . ') ' . $width . 'px';
-      if ( $width <= $small )
-        $queries[] = '(min-width: ' . $small . ') ' . $width . 'px';
+      $queries[] = '(min-width: ' . $full . 'px) ' . $content_width . 'px';
     }
 
   } else {
 
-    // Above the medium breakpoint most images will max out at $main_width
-    if ( empty( $width ) || $width >= $main_width ) {
-      $queries[] = '(min-width: ' . $medium . ') ' . $main_width . 'px';
+    if ( !empty( $width ) ) {
+
+      // Divide
+      if ( $width < $main_width )
+        $width = $width * ( $main_width / $content_width );
+
+      // Approach: start with the largest media query and move down as long as the image width is less than one breakpoint and more than the next
+      if ( $width > $medium ) {
+        $queries[] = '(min-width: ' . ( $width + ( PENDRELL_BASELINE * 3 ) ) . 'px) ' . $width . 'px';
+      } elseif ( $width <= $medium && $width > $small ) {
+        $queries[] = '(min-width: ' . ( $width + ( PENDRELL_BASELINE * 2 ) ) . 'px) ' . $width . 'px';
+      } else {
+        $queries[] = '(min-width: ' . ( $width + PENDRELL_BASELINE ) . 'px) ' . $width . 'px';
+      }
     } else {
-      // 0.65 is the difference between $content_width and $main_width
-      $queries[] = '(min-width: ' . $medium . ') ' . $width * 0.65 . 'px';
-      if ( $width <= $small )
-        $queries[] = '(min-width: ' . $small . ') ' . $width * 0.65 . 'px';
+      $queries[] = '(min-width: ' . $medium . 'px) ' . $main_width . 'px';
     }
   }
 
@@ -58,8 +70,11 @@ add_filter( 'ubik_imagery_sizes_media_queries', 'pendrell_sizes_media_queries', 
 if ( !function_exists( 'pendrell_sizes_default' ) ) : function pendrell_sizes_default( $default ) {
 
   // Allows another function to override this one
-  if ( empty( $default ) )
-    $default = 'calc(100vw - ' . PENDRELL_BASELINE . 'px)'; // An attempt to account for page margins; use $default = '100vw' if this seems too complicated
+  if ( empty( $default ) ) {
+    // An attempt to account for page margins; use $default = '100vw' if this seems too complicated
+    $default = 'calc(100vw - ' . PENDRELL_BASELINE . 'px)'; // `calc()` support: http://caniuse.com/#search=calc
+  }
+
 
   // Return the default string
   return $default;
