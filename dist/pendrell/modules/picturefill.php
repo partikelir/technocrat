a5f6cdd457c065e36e41e0455a9b01bfe4ac77b4
Alexander Synaptic <alexander@synapticism.com>
2014-12-05 17:22:53 +0800

    A working implementation of the `sizes` attribute

--- dist/pendrell/modules/picturefill.php
+++ dist/pendrell/modules/picturefill.php
@@ -1,12 +1,9 @@
 <?php // ==== PICTUREFILL ==== //
 
 // Setup a few media queries for the `sizes` attribute; must be an array even if there is only one value
-if ( !function_exists( 'pendrell_sizes_media_queries' ) ) : function pendrell_sizes_media_queries( $queries ) {
+if ( !function_exists( 'pendrell_sizes_media_queries' ) ) : function pendrell_sizes_media_queries( $queries = array(), $width = '' ) {
 
   global $content_width, $main_width;
-
-  // Flush the array? The order of the `sizes` attributes matters but this prevents us from hooking the same filter elsewhere
-  $queries = array();
 
   // Breakpoints replicated from `src/scss/_base_config.scss`
   $full = $content_width + ( PENDRELL_BASELINE * 3) . 'px';
@@ -14,23 +11,42 @@ if ( !function_exists( 'pendrell_sizes_media_queries' ) ) : function pendrell_si
   $medium = $main_width + ( PENDRELL_BASELINE * 3) . 'px';
   $small = $main_width/1.2 . 'px';
 
+  // Note: the order of media queries is important, for the browser will go with the first match!
+
   // Full-width is a special case and requires its own media queries
-  if ( !pendrell_is_full_width() ) {
+  if ( pendrell_is_full_width() ) {
+
     // Above the full breakpoint full-width images will max out at $content_width
-    $queries[] = '(min-width: ' . $full . ') ' . $content_width . 'px';
+    if ( empty( $width ) || $width >= $content_width ) {
+      $queries[] = '(min-width: ' . $full . ') ' . $content_width . 'px';
+    } else {
+      $queries[] = '(min-width: ' . $full . ') ' . $width . 'px';
+      if ( $width <= $large )
+        $queries[] = '(min-width: ' . $large . ') ' . $width . 'px';
+      if ( $width <= $medium )
+        $queries[] = '(min-width: ' . $medium . ') ' . $width . 'px';
+      if ( $width <= $small )
+        $queries[] = '(min-width: ' . $small . ') ' . $width . 'px';
+    }
+
   } else {
+
     // Above the medium breakpoint most images will max out at $main_width
-    $queries[] = '(min-width: ' . $medium . ') ' . $main_width . 'px';
+    if ( empty( $width ) || $width >= $main_width ) {
+      $queries[] = '(min-width: ' . $medium . ') ' . $main_width . 'px';
+    } else {
+      $queries[] = '(min-width: ' . $medium . ') ' . $width . 'px';
+    }
   }
 
   // Note: below the previously specified breakpoints most images will expand to fill the window (minus margins)
   // For this reason no further media queries are specified; the default "100vw" is good enough to handle these cases
-  // @TODO: handle half/third/quarter width images and similar layouts
 
-  return $queries;
+  // Return an array of arrays
+  return array( $queries );
 
 } endif;
-add_filter( 'ubik_imagery_sizes_media_queries', 'pendrell_sizes_media_queries' );
+add_filter( 'ubik_imagery_sizes_media_queries', 'pendrell_sizes_media_queries', 10, 2 );
 
 
 
