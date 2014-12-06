437acb256e64a8af2c432b14c5d1865058a7ca24
Alexander Synaptic <alexander@synapticism.com>
2014-12-07 04:07:23 +0800

    Picturefill working draft

--- dist/pendrell/modules/picturefill.php
+++ dist/pendrell/modules/picturefill.php
@@ -1,6 +1,17 @@
 <?php // ==== PICTUREFILL ==== //
 
-// Setup a few media queries for the `sizes` attribute; must be an array even if there is only one value
+// Notes about the `sizes` attribute:
+// - The *order* of media queries is important; browsers pick the first match so start with the largest query!
+// - The purpose of the `sizes` attribute is to give the browser an accurate estimation of the *rendered size* of an image at different viewport widths
+// - The approach taken will depend entirely on how images are displayed in the theme
+// - In the case of this theme, Pendrell, there are several complications:
+//   - The use of fractional width images (half/third/quarter-width)
+//   - The use of a full-width template
+//   - Page margins that vary according to viewport width
+//   - Gallery view switches between a two and three column layout (still @TODO)
+// These scenarious will be discussed in the code below...
+
+// Setup a few media queries for the `sizes` attribute; must be an array even if there is only one value!
 if ( !function_exists( 'pendrell_sizes_media_queries' ) ) : function pendrell_sizes_media_queries( $queries = array(), $size = '', $width = '' ) {
 
   // Exit early if we don't have the required size and width data
@@ -9,56 +20,73 @@ if ( !function_exists( 'pendrell_sizes_media_queries' ) ) : function pendrell_si
 
   global $content_width, $main_width;
 
-  // Media queries for this theme should be fairly straight-forward as the layout is fairly simple
-  // Either content is displayed with a sidebar or it's all displayed in a single column
-  // The main thing that varies is the margins (which get progressively smaller as the viewport width decreases)
-  // One special case: half/third/quarter-width images require a slightly more refined approach
-  // Note: the *order* of media queries is important; browsers pick the first match!
+  // Get the margin from the main configuration file; 30px
+  $margin = (int) PENDRELL_BASELINE;
 
   // Breakpoints replicated from `src/scss/_base_config.scss`
-  $full   = $content_width + ( PENDRELL_BASELINE * 3);
-  $large  = $main_width + ( PENDRELL_BASELINE * 8);
-  $medium = $main_width + ( PENDRELL_BASELINE * 3);
-  $small  = ceil( $main_width/1.2 );
+  $small = ceil( $main_width/1.2 ) - ( $margin * 2 ); // 460px
+  $small_padded = $small + ( $margin * 2 ); // 520px
+  $medium = $main_width; // 624px
+  $medium_padded = $medium + ( $margin * 3 ); // 714px
 
-  // @TODO: Houston, we have a problem
+  // Fractional width sizes, a complicated example of calculating the `sizes` attribute...
+  if ( in_array( $size, array( 'half', 'half-square', 'third', 'third-square', 'quarter', 'quarter-square' ) ) ) {
 
-  // Full-width is a special case and requires its own media queries
-  if ( pendrell_is_full_width() ) {
+    // Multiplier for fractional width sizes
+    $factor = 1;
+    if ( in_array( $size, array( 'half', 'half-square' ) ) )
+      $factor = 2;
+    if ( in_array( $size, array( 'third', 'third-square' ) ) )
+      $factor = 3;
+    if ( in_array( $size, array( 'quarter', 'quarter-square' ) ) )
+      $factor = 4;
 
-    // Approach: start with the largest media query and move down as long as the image width is less than one breakpoint and more than the next
-    // What we want to specify is the minimum width at which an image is reliably going to scale to its target width
-    // So, for instance, if we have a 624px image (the default medium size), the minimum width for full display will be 624px + margins
-    // However, if our target image is grouped (e.g. in a set of two, three, four, or more)
+    // This works but it's confusing as fuck; @TODO: document this!
+    $viewport = round( 100 / $factor, 5 );
+    $margin_inner = $margin * ( $factor - 1 );
+    $combined_width = ( $width * $factor ) + $margin_inner;
 
-    if ( $width > $medium ) {
-      $queries[] = '(min-width: ' . ( $width + ( PENDRELL_BASELINE * 3 ) ) . 'px) ' . $width . 'px';
-    } elseif ( $width <= $medium && $width > $small ) {
-      $queries[] = '(min-width: ' . ( $width + ( PENDRELL_BASELINE * 2 ) ) . 'px) ' . $width . 'px';
+    if ( $combined_width > $medium ) {
+      $queries[] = '(min-width: ' . ( $combined_width + ( $margin * 3 ) ) . 'px) ' . $width . 'px';
+      $queries[] = '(min-width: ' . $medium_padded . 'px) calc(' . $viewport . 'vw - ' . round( ( ( ( $margin * 3 ) + $margin_inner ) / $factor ), 5 ) . 'px)';
+      $queries[] = '(min-width: ' . $small_padded . 'px) calc(' . $viewport . 'vw - ' . round( ( ( ( $margin * 2 ) + $margin_inner ) / $factor ), 5 ) . 'px)';
+    } elseif ( $combined_width > $small ) {
+      $queries[] = '(min-width: ' . ( $combined_width + ( $margin * 2 ) ) . 'px) ' . $width . 'px';
+      $queries[] = '(min-width: ' . $small_padded . 'px) calc(' . $viewport . 'vw - ' . round( ( ( ( $margin * 2 ) + $margin_inner ) / $factor ), 5 ) . 'px)';
     } else {
-      $queries[] = '(min-width: ' . ( $width + PENDRELL_BASELINE ) . 'px) ' . $width . 'px';
+      $queries[] = '(min-width: ' . ( $combined_width + $margin ) . 'px) ' . $width . 'px';
     }
 
   } else {
 
-    // Divide and conquer
-    if ( $width < $main_width )
-      $width = $width * ( $main_width / $content_width );
-
-    // Approach: start with the largest media query and move down as long as the image width is less than one breakpoint and more than the next
-    if ( $width > $medium ) {
-      $queries[] = '(min-width: ' . ( $width + ( PENDRELL_BASELINE * 3 ) ) . 'px) ' . $width . 'px';
-    } elseif ( $width <= $medium && $width > $small ) {
-      $queries[] = '(min-width: ' . ( $width + ( PENDRELL_BASELINE * 2 ) ) . 'px) ' . $width . 'px';
+    // Maximum rendered image size for the theme; only applies to the non-fractional image size case
+    if ( pendrell_is_full_width() ) {
+      $width = min( $width, $content_width ); // Can't render any larger than $content_width
     } else {
-      $queries[] = '(min-width: ' . ( $width + PENDRELL_BASELINE ) . 'px) ' . $width . 'px';
+      $width = min( $width, $main_width ); // Can't render any larger than $main_width
+    }
+
+    // The following media queries handle all images intended to fill the content area of this theme
+    // The topmost breakpoint is set to the width of the image plus known page margins
+    // When this media query is satisfied the image is displayed at its original intended width
+    // Below this point the image will naturally expand to fill available space
+    // However: the rendered size will *not* be '100vw' (i.e. 100% of the viewport) due to the margins around the content area
+    // These margins change according to breakpoints set in `src/scss/_base_config.scss`
+    // Consequently, the viewport width is modified to account for the changing margins at various breakpoints
+    // Finally, the default media query (defined below) handles all other scenarios
+    if ( $width > $medium ) {
+      $queries[] = '(min-width: ' . ( $width + ( $margin * 3 ) ) . 'px) ' . $width . 'px';
+      $queries[] = '(min-width: ' . $medium_padded . 'px) calc(100vw - ' . ( $margin * 3 ) . 'px)';
+      $queries[] = '(min-width: ' . $small_padded . 'px) calc(100vw - ' . ( $margin * 2 ) . 'px)';
+    } elseif ( $width > $small ) {
+      $queries[] = '(min-width: ' . ( $width + ( $margin * 2 ) ) . 'px) ' . $width . 'px';
+      $queries[] = '(min-width: ' . $small_padded . 'px) calc(100vw - ' . ( $margin * 2 ) . 'px)';
+    } else {
+      $queries[] = '(min-width: ' . ( $width + $margin ) . 'px) ' . $width . 'px';
     }
   }
 
-  // Note: below the previously specified breakpoints most images will expand to fill the window (minus margins)
-  // For this reason no further media queries are specified; the default (below) is good enough to handle these cases
-
-  // Return an array of arrays
+  // Return an array of arrays (required by Ubik Imagery)
   return array( $queries );
 
 } endif;
@@ -69,25 +97,33 @@ add_filter( 'ubik_imagery_sizes_media_queries', 'pendrell_sizes_media_queries', 
 // Filter the default `sizes` attribute (which is otherwise blank)
 if ( !function_exists( 'pendrell_sizes_default' ) ) : function pendrell_sizes_default( $default = '', $size = '', $width = '' ) {
 
-  // Default viewport width
+  // Default viewport width and (optionally) a margin; both integers
   $viewport = 100;
+  $margin = (int) PENDRELL_BASELINE;
 
-  // Divide the default viewport width for half/third/quarter-width images
+  // Multiplier
+  $factor = 1;
   if ( in_array( $size, array( 'half', 'half-square' ) ) )
-    $viewport = round( $viewport / 2, 5);
+    $factor = 2;
   if ( in_array( $size, array( 'third', 'third-square' ) ) )
-    $viewport = round( $viewport / 3, 5);
+    $factor = 3;
   if ( in_array( $size, array( 'quarter', 'quarter-square' ) ) )
-    $viewport = round( $viewport / 4, 5);
+    $factor = 4;
 
-  // Allows another function to override this one
-  if ( empty( $default ) ) {
-
-    // An attempt to account for page margins; use $default = '100vw' if this seems too complicated
-    $default = 'calc(' . $viewport . 'vw - ' . PENDRELL_BASELINE . 'px)'; // `calc()` support: http://caniuse.com/#search=calc
+  // Special handling for fractional width images
+  if ( $factor > 1 ) {
+    $viewport = round( 100 / $factor, 5 ); // Divide the default viewport width for half/third/quarter-width images
+    $margin = ( $margin + ( $margin * ( $factor - 1 ) ) ) / $factor; // Um... trust me
   }
 
-  // Return the default string
+  // Margins in this theme vary according to viewport size; what we want here is the smallest possible margin
+  if ( !empty( $margin ) ) {
+    $default = 'calc(' . $viewport . 'vw - ' . $margin . 'px)'; // `calc()` support: http://caniuse.com/#search=calc
+  } else {
+    $default = $viewport . 'vw'; // Without margins we'll just assume that images take up the full viewport on smaller screens
+  }
+
+  // Return the default `sizes` attribute
   return $default;
 
 } endif;
