b5dc115dd4d0797f020bd48f2867b4c6e35dd3d0
Alexander Synaptic <alexander@synapticism.com>
2015-02-22 01:20:26 +0800

    Sizes attributes generation redux

--- src/modules/ubik-imagery.php
+++ src/modules/ubik-imagery.php
@@ -43,57 +43,61 @@ function pendrell_sizes_media_queries( $queries = array(), $size = '', $width = 
     $bounding_width = $main_width;
   }
 
-  // Let's limit the requested width to the bounding box since we're interested in rendered size
+  // Limit width by the bounding width; what we're interested in here is the *rendered size* of an image which won't be larger than the container
   $width = min( $width, $bounding_width );
 
-  // Test the context object for various scenarios
+  // Test the context object for various scenarios; this allows the theme to handle the sizes attribute in different ways depending on how images are being used
   $group        = pendrell_sizes_context( $context, 'group' );
   $responsive   = pendrell_sizes_context( $context, 'responsive' );
   $static       = pendrell_sizes_context( $context, 'static' );
 
   // The margins can be filtered; this is mostly in case the inner margin (the space *between* grouped images) is not the same as the page margins
   // Example: your outer page margins are 30px on each side but the spacing between images is 20px
-  $margin           = pendrell_sizes_margin();
-  $margin_inner     = pendrell_sizes_margin_inner();
+  // Such functionality is superfluous for this theme as margins are all handled in increments of 30px
+  $margin         = pendrell_sizes_margin();
+  $margin_inner   = pendrell_sizes_margin_inner();
 
   // Breakpoints replicated from `src/scss/config/_settings.scss`
-  $b_tiny           = ceil( $main_width / 1.5 );                    // 390px
-  $b_small          = ceil( $main_width / 1.2 );                    // 520px
-  $b_medium         = $main_width + ( $margin * 3 );                // 714px
+  $tiny           = ceil( $main_width / 1.5 );                    // 390px
+  $small          = ceil( $main_width / 1.2 );                    // 520px
+  $medium         = $main_width + ( $margin * 3 );                // 714px
+  $large          = $main_width + ( $margin * 8 );                // 864px
+  $full           = $content_width + ( $margin * 3 );             // 1050px
 
   // Usable space for each breakpoint; for comparison with $width
-  $b_tiny_content   = ceil( $main_width / 1.5 ) - $margin;          // 360px
-  $b_small_content  = ceil( $main_width / 1.2 ) - ( $margin * 2 );  // 460px
-  $b_medium_content = $main_width;                                  // 624px
+  $tiny_content   = ceil( $main_width / 1.5 ) - $margin;          // 360px
+  $small_content  = ceil( $main_width / 1.2 ) - ( $margin * 2 );  // 460px
+  $medium_content = $main_width;                                  // 624px
+  $large_content  = $main_width + ( $margin * 5 );                // 774px
 
-  // Fractional width sizes, a complicated example of calculating the `sizes` attribute
+  // This first chunk of code deals with grouped fractional width images
   // These sizes are defined as fractions of $content_width by `ubik_imagery_add_fractional_sizes()`
   // Note: this only works when images are explicitly grouped (i.e. with the `[group]` shortcode or by passing `group` to the context object when calling `ubik_imagery`); otherwise they are treated like any other image
   if ( $group === true || $responsive === true ) {
 
-    // Multiplier for use with fractional width sizes`
+    // Multiplier for use with fractional width sizes
     $factor = 2; // $group is true so we expect two images in a row by default
     if ( in_array( $size, array( 'third', 'third-square' ) ) )
       $factor = 3;
     if ( in_array( $size, array( 'quarter', 'quarter-square' ) ) )
       $factor = 4;
 
-    // We lead with a media query specifying the minimum viewport width at which an image is *fixed* in size (not fluid)
+    // We lead with a media query specifying the minimum pixel width at which an image is *fixed* in size (not fluid)
     // In this theme only images displayed in full-width mode will render at the requested width ($width); everything else will be fixed but downsized
     // As such we test whether this is a full-width view and, if not, attempt to calculate the downsized width given a smaller $bounding_width
     if ( !pendrell_is_full_width() )
       $width = round( ( $bounding_width - ( $margin_inner * ( $factor - 1 ) ) ) / $factor, 5 );
-    $queries[] = '(min-width: ' . ( $bounding_width + ( $margin * 3 ) ) . 'px) ' . $width . 'px';
+    $queries[] = '(min-width: ' . $full . 'px) ' . $width . 'px';
 
-    // A regular grouped gallery
+    // A regular grouped gallery; a helper class like `gallery-columns-3` and the corresponding image size (e.g. `third-square`) must be passed to Ubik Imagery
     if ( $responsive === false ) {
 
-      // The next media query handles the medium breakpoint
+      // This next media query handles the medium breakpoint
       // By this point all images are rendered smaller than their intrinsic sizes and will be sized based on a percentage width of the viewport minus page margins and margins between images within a group
       // For example: third-width images will take up one third of the viewport width minus one third of the total width of the inner margins (of which there will be two in this case) minus the *fixed* width of the page margins (hence the use of `calc`)
       // Note: these values won't add up to 100 due to the presence of the inner margins
       $viewport = round( ( 1 / $factor - ( ( ( $margin_inner * ( $factor - 1 ) ) / $bounding_width ) ) / $factor ) * 100, 5 );
-      $queries[] = '(min-width: ' . $b_medium . 'px) calc(' . $viewport . 'vw - ' . round( ( $margin * 3 ) / $factor, 5 ) . 'px)';
+      $queries[] = '(min-width: ' . $medium . 'px) calc(' . $viewport . 'vw - ' . round( ( $margin * 3 ) / $factor, 5 ) . 'px)';
 
       // Special case: for non-static galleries the 4 column layout collapses into a 2 column layout below the medium breakpoint
       if ( $static === false && $factor === 4 ) {
@@ -102,39 +106,59 @@ function pendrell_sizes_media_queries( $queries = array(), $size = '', $width = 
       }
 
       // The 2 and 3 column layouts only need to have the page margins progressively reduced; everything else remains the same
-      $queries[] = '(min-width: ' . $b_small . 'px) calc(' . $viewport . 'vw - ' . round( ( $margin * 2 ) / $factor, 5 ) . 'px)';
-      $queries[] = '(min-width: ' . $b_tiny . 'px) calc(' . $viewport . 'vw - ' . round( $margin / $factor, 5 ) . 'px)';
+      $queries[] = '(min-width: ' . $small . 'px) calc(' . $viewport . 'vw - ' . round( ( $margin * 2 ) / $factor, 5 ) . 'px)';
+      $queries[] = '(min-width: ' . $tiny . 'px) calc(' . $viewport . 'vw - ' . round( $margin / $factor, 5 ) . 'px)';
 
-    // Responsive gallery handling
+    // Responsive gallery handling; no need to specify a column count or work with factors here
+    // This is a special case that allows for a portfolio-like tiling of images in rows of 3, 2, or 1 image depending on the size of the viewport window
+    // Naturally this works best with posts per page equally divisible by all three factors e.g. 6, 12, 18, etc.
     } else {
 
       // Medium:  3x columns, 3x page margins
       // Small:   2x columns, 2x page margins
       // Tiny:    2x columns, 1x page margins
       // Default: 1x columns, 1x page margins; handled by `pendrell_sizes_default()`
-      $queries[] = '(min-width: ' . $b_medium . 'px) calc(' . round( ( 1 / 3 - ( ( ( $margin_inner * 2 ) / $content_width ) ) / 3 ) * 100, 5 ) . 'vw - ' . round( ( $margin * 3 ) / 3, 5 ) . 'px)';
-      $queries[] = '(min-width: ' . $b_small . 'px) calc(' . round( ( 1 / 2 - ( ( $margin_inner / $content_width ) ) / 2 ) * 100, 5 ) . 'vw - ' . round( ( $margin * 2 ) / 2, 5 ) . 'px)';
-      $queries[] = '(min-width: ' . $b_tiny . 'px) calc(' . round( ( 1 / 2 - ( ( $margin_inner / $content_width ) ) / 2 ) * 100, 5 ) . 'vw - ' . round( $margin / 2, 5 ) . 'px)';
+      $queries[] = '(min-width: ' . $medium . 'px) calc(' . round( ( 1 / 3 - ( ( ( $margin_inner * 2 ) / $content_width ) ) / 3 ) * 100, 5 ) . 'vw - ' . round( ( $margin * 3 ) / 3, 5 ) . 'px)';
+      $queries[] = '(min-width: ' . $small . 'px) calc(' . round( ( 1 / 2 - ( ( $margin_inner / $content_width ) ) / 2 ) * 100, 5 ) . 'vw - ' . round( ( $margin * 2 ) / 2, 5 ) . 'px)';
+      $queries[] = '(min-width: ' . $tiny . 'px) calc(' . round( ( 1 / 2 - ( ( $margin_inner / $content_width ) ) / 2 ) * 100, 5 ) . 'vw - ' . round( $margin / 2, 5 ) . 'px)';
     }
   } else {
 
-    // This block handles the most basic responsive images scenario: single images, presumably filling available space
-    // The topmost breakpoint is set to the width of the image plus known page margins
-    // When this media query is satisfied the image is displayed at its original intended width
-    // Below this point the image will naturally expand to fill available space
-    // However: the rendered size will *not* be '100vw' (i.e. 100% of the viewport) due to the margins around the content area
-    // These margins change according to breakpoints set in `src/scss/config/_settings.scss`
-    // Consequently, the viewport width is modified to account for the changing margins at various breakpoints using `calc`
-    // Note: it is also necessary to specify a default for the `sizes` attribute; have a look at the next function for an example
-    if ( $width >= $b_medium_content ) {
-      $queries[] = '(min-width: ' . ( $width + ( $margin * 3 ) ) . 'px) ' . $width . 'px';
-      $queries[] = '(min-width: ' . $b_medium . 'px) calc(100vw - ' . ( $margin * 3 ) . 'px)';
-      $queries[] = '(min-width: ' . $b_small . 'px) calc(100vw - ' . ( $margin * 2 ) . 'px)';
-    } elseif ( $width >= $b_small_content ) {
-      $queries[] = '(min-width: ' . ( $width + ( $margin * 2 ) ) . 'px) ' . $width . 'px';
-      $queries[] = '(min-width: ' . $b_small . 'px) calc(100vw - ' . ( $margin * 2 ) . 'px)';
+    // This block handles the most basic responsive images scenario: single images without any specific layout
+    // Regular and full-width display require slightly different approaches
+    // As before, the first media query must specify the minimum width at which an image is rendered at the *requested* width
+    // Page margins also introduce some ambiguity at the small and medium breakpoints
+    // Note: viewport calculations will *not* add up to 100 due to the presence of margins around the content area
+    if ( pendrell_is_full_width() ) {
+      if ( ( $width + ( $margin * 2 ) ) > $medium ) {
+        $queries[] = '(min-width: ' . ( $width + ( $margin * 3 ) ) . 'px) ' . $width . 'px';
+        $queries[] = '(min-width: ' . $medium . 'px) calc(100vw - ' . ( $margin * 3 ) . 'px)';
+        $queries[] = '(min-width: ' . $small . 'px) calc(100vw - ' . ( $margin * 2 ) . 'px)';
+      } elseif ( ( $width + $margin ) > $small ) {
+        $queries[] = '(min-width: ' . ( $width + ( $margin * 2 ) ) . 'px) ' . $width . 'px';
+        $queries[] = '(min-width: ' . $small . 'px) calc(100vw - ' . ( $margin * 2 ) . 'px)';
+      } else {
+        $queries[] = '(min-width: ' . ( $width + $margin ) . 'px) ' . $width . 'px';
+      }
     } else {
-      $queries[] = '(min-width: ' . ( $width + $margin ) . 'px) ' . $width . 'px';
+
+      // Calculate the usable space on the cusp of the large breakpoint in when not in full-width mode
+      // This will determine whether we add a couple of extra media queries to the top of the stack
+      // These extra queries handle things when the main content and sidebar are starting to squish together and the sidebar hasn't yet dropped down to the bottom
+      $large_usable = floor( ( $large_content - ( $margin * 2 ) ) * ( ( $main_width + $margin ) / $content_width ) ); // Works out to around 487px
+      if ( $width >= $large_usable ) {
+        $queries[] = '(min-width: ' . $full . 'px) ' . $width . 'px';
+        $queries[] = '(min-width: ' . $large . 'px) calc(' . round( ( $main_width / ( $content_width - $margin * 2 ) ) * 100, 5 ) . 'vw - ' . ( $margin * 5 ) . 'px)';
+      }
+
+      // Now we must output another media query declaring at what width an image will be displayed at the requested size
+      // Note: all images not displayed in full-width mode will top out at 624px meaning we only need to concern ourself with the small breakpoint (from 2x page margins to 1x page margins)
+      if ( ( $width + $margin ) > $small ) {
+        $queries[] = '(min-width: ' . ( $width + ( $margin * 2 ) ) . 'px) ' . $width . 'px';
+        $queries[] = '(min-width: ' . $small . 'px) calc(100vw - ' . ( $margin * 2 ) . 'px)';
+      } else {
+        $queries[] = '(min-width: ' . ( $width + $margin ) . 'px) ' . $width . 'px';
+      }
     }
   }
 
