fe6612645ad79c228b030aadca27eec4de2beefc
Alexander Synaptic <alexander@synapticism.com>
2015-03-14 04:58:49 +0800

    Footer fix, widget list item spacing

--- src/footer.php
+++ src/footer.php
@@ -12,15 +12,17 @@
 ?>
       </div>
       <div id="wrap-footer" class="wrap-footer">
-      	<nav id="site-footer-navigation" role="navigation">
-          <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'menu-footer inline-menu' ) ); ?>
-        </nav>
-        <div class="site-footer-buttons">
-          <a href="#top" class="button" rel="nofollow"><?php echo pendrell_icon( 'top-link', __( 'Top', 'pendrell' ) ); ?></a>
+        <div class="site-footer">
+        	<nav id="site-footer-navigation" role="navigation">
+            <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'menu-footer inline-menu' ) ); ?>
+          </nav>
+          <div class="site-footer-buttons">
+            <a href="#top" class="button" rel="nofollow"><?php echo pendrell_icon( 'top-link', __( 'Top', 'pendrell' ) ); ?></a>
+          </div>
+          <footer id="colophon" class="site-colophon" role="contentinfo">
+            <?php do_action( 'pendrell_footer' ); ?>
+        	</footer>
         </div>
-        <footer id="colophon" class="site-footer" role="contentinfo">
-          <?php do_action( 'pendrell_footer' ); ?>
-      	</footer>
       </div>
     </div>
   <?php wp_footer(); ?>
