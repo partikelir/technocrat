517697c23687f2269dce8afac9533b385c61a8f9
Alexander Synaptic <alexander@synapticism.com>
2015-03-31 20:42:07 +0800

    Icon fixes

--- src/js/core.js
+++ src/js/core.js
@@ -3,6 +3,6 @@
 // Anything entered here will end up at the top of `p-core.js`
 ;(function($){
   $(function(){ // Shortcut to $(document).ready(handler);
-    autosize(document.querySelectorAll('textarea'));
+    autosize(document.querySelectorAll('textarea')); // Autosizes textareas based on user input
   });
 }(jQuery));
