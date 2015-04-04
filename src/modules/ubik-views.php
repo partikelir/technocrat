717d5dca5ff88d98edc69dd969c232f7b6b8ce19
Alexander Synaptic <alexander@synapticism.com>
2015-04-04 15:48:20 +0800

    Small views update

--- src/modules/ubik-views.php
+++ src/modules/ubik-views.php
@@ -27,7 +27,6 @@ function pendrell_views_buttons( $buttons ) {
   $links = ubik_views_links();
   if ( !empty( $links ) ) {
     foreach ( $links as $view => $data ) {
-      //$buttons .= '<a class="button view-link" href="' . $data['link'] . '" rel="nofollow" role="button">' . pendrell_icon( 'view-' . $view, $data['name'] ) . '</a>';
       $buttons .= '<a class="button view-link" href="' . $data['link'] . '" rel="nofollow" role="button">' . $data['name'] . '</a>';
     }
   }
@@ -39,8 +38,6 @@ add_filter( 'pendrell_archive_buttons', 'pendrell_views_buttons', 5 );
 
 // Do not display views navigation on certain terms
 function pendrell_views_buttons_display( $switch ) {
-  if ( is_tag( array( 'culture-log', 'development-log', 'photography-log' ) ) )
-    $switch = false;
   return $switch;
 }
 add_filter( 'ubik_views_links_display', 'pendrell_views_buttons_display' );
@@ -49,7 +46,7 @@ add_filter( 'ubik_views_links_display', 'pendrell_views_buttons_display' );
 
 // View content class filter; adds classes to the main content element rather than body class (for compatibility with the full-width module)
 function pendrell_views_content_class( $classes ) {
-  if ( ubik_is_view( 'gallery' ) )
+  if ( ubik_views_test( 'gallery' ) )
     $classes[] = 'gallery gallery-flex';
   return $classes;
 }
@@ -59,7 +56,7 @@ add_filter( 'pendrell_content_class', 'pendrell_views_content_class' );
 
 // Force certain views to be full-width
 function pendrell_views_full_width( $full_width_test ) {
-  if ( ubik_is_view( 'gallery' ) )
+  if ( ubik_views_test( 'gallery' ) )
     return true;
   return $full_width_test;
 }
@@ -69,7 +66,7 @@ add_filter( 'pendrell_full_width', 'pendrell_views_full_width' );
 
 // Modify how many posts per page are displayed for different views; adapted from: http://wordpress.stackexchange.com/questions/21/show-a-different-number-of-posts-per-page-depending-on-context-e-g-homepage
 function pendrell_views_pre_get_posts( $query ) {
-  if ( ubik_is_view( 'gallery' ) ) {
+  if ( ubik_views_test( 'gallery' ) ) {
     $query->set( 'ignore_sticky_posts', true );
     $query->set( 'posts_per_page', 18 ); // Best if this is a number divisible by both 2 and 3
   }
@@ -82,12 +79,12 @@ add_action( 'pre_get_posts', 'pendrell_views_pre_get_posts' );
 function pendrell_views_template_part( $name ) {
 
   // Get the current view
-  $view = ubik_views_current();
+  $current = ubik_views_current();
 
   // Assign a template name unless the view is empty or not the default 'posts' view
-  if ( !empty( $view ) ) {
-    if ( $view !== 'posts' )
-      $name = $view;
+  if ( !empty( $current ) ) {
+    if ( $current !== 'posts' )
+      $name = $current;
   }
   return $name;
 }
@@ -97,7 +94,7 @@ add_filter( 'pendrell_template_part', 'pendrell_views_template_part' );
 
 // Entry meta for list view, called directly from the template
 function pendrell_views_list_meta( $contents ) {
-  if ( ubik_is_view( 'list' ) )
+  if ( ubik_views_test( 'list' ) )
     $contents = ubik_meta_date_published( _x( 'F j, Y', 'list view date format', 'pendrell' ) ); //strip_tags( $date[0], '<span><time>' ); // Publication date with any potential links stripped
   return $contents;
 }
@@ -116,7 +113,7 @@ function pendrell_views_list_content( $words = 15 ) {
 
 // Switch for next and previous pages
 function pendrell_views_nav_content_switch( $switch, $id ) {
-  if ( $id === 'nav-above' && ubik_is_view( 'gallery' ) )
+  if ( $id === 'nav-above' && ubik_views_test( 'gallery' ) )
     $switch = false;
   return $switch;
 }
