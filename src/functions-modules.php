076d4d3f313eeaa458ab91627cb35c4b5aa872f7
Alexander Synaptic <alexander@synapticism.com>
2015-04-29 12:02:28 +0800

    Remove emoji

--- src/functions-modules.php
+++ src/functions-modules.php
@@ -63,6 +63,7 @@ if ( PENDRELL_UBIK_ANALYTICS )
 
 // == CLEANER * == //
 
+define( 'UBIK_CLEANER_REMOVE_EMOJI', true );
 define( 'UBIK_CLEANER_REMOVE_MIGRATE', true );
 define( 'UBIK_CLEANER_REMOVE_OPEN_SANS', true );
 define( 'UBIK_CLEANER_STYLE_TEMPLATES', true );
