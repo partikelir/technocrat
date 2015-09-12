b32bc8877b7f4e8cd2c05f1be0a2a6e3b18250e5
Alexander Synaptic <alexander@synapticism.com>
2015-09-12 17:48:18 +0800

    Magnific integration updates

--- src/js/magnific-popup-2.js
+++ src/js/magnific-popup-2.js
@@ -12,7 +12,9 @@
     var magnificOptions = {
       disableOn: 500, // Don't load the popup gallery on screens with a viewport width less than this
       delegate: 'figure.wp-caption', // Open the popup by clicking on child elements matching this selector
+      key: 'mfp-main',
       overflowY: 'scroll', // Assumes there is a scrollbar; prevents elements from shifting around in Chrome and perhaps other browsers
+      closeBtnInside: false,
       showCloseBtn: false,
       type: 'image',
       image: {
@@ -25,9 +27,10 @@
         enabled: true,
         //preload: [0,2], // Lazy loading options
         arrowMarkup: '<div class="mfp-arrow mfp-arrow-%dir%">' + svgIcon( 'typ-arrow-%dir%-thick', '%title%', '%title%' ) + '</div>',
-        tPrev: 'Previous', // title for left button
-        tNext: 'Next', // title for right button
-        tCounter: '<span class="mfp-counter">%curr%/%total%</span>'
+        navigateByImgClick: false,
+        tCounter: '%curr%/%total%',
+        tNext: 'Next',
+        tPrev: 'Previous'
       },
       callbacks: {
         open: function() {
@@ -42,22 +45,32 @@
           // @TODO: display a different image size from `srcset` attribute as needed
         },
         imageLoadComplete: function() {
-          // Fires when image in current popup finished loading; use this to remove spinner
-        },
-        markupParse: function(template, values, item) {
-          // Parse markup
+
+          // Get the image and link (if available)
+          var img = $('.mfp-img'),
+              link = this.currItem.el.find('a:first').clone().empty();
+
+          // Remove any existing link
+          if ( img.parent().is('a') ) {
+            img.unwrap();
+          }
+
+          // Add the link from the body of the post
+          if (typeof link !== 'undefined') {
+            img.wrap(link);
+          }
         },
         elementParse: function(item) {
 
-          // Use `srcset` to determine what image to load based on current viewport width; @TODO: retina support
-          item.srcset = item.el.find('img').attr('srcset');
-          if ( item.srcset ) {
+          // Use `srcset` to determine what image to load based on current viewport width; @TODO: retina support?
+          var srcset = item.el.find('img').attr('srcset');
+          if (srcset.length) {
 
             // Set target width; https://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript
             var targetWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
             // Parse `srcset` attribute; https://github.com/albell/parse-srcset
-            var imagesAvailable = parseSrcset( item.srcset );
+            var imagesAvailable = parseSrcset( srcset );
 
             // Sort backwards from smallest to largest and cycle through to find an appropriate image choice
             imagesAvailable.sort( function(a, b) {
@@ -76,7 +89,8 @@
         }
       }
     };
-    $('.entry-content').magnificPopup(magnificOptions);
 
+    // Engage
+    $('.entry-content').magnificPopup(magnificOptions);
   });
 }(jQuery));
