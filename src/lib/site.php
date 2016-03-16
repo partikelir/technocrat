cc9f7d15f6ab5bf9a9659ff69727e504b7cd7a78
Alexander Synaptic <alexander@synapticism.com>
2016-03-16 20:02:42 +0800

    Header buttons styling hooks

--- src/lib/site.php
+++ src/lib/site.php
@@ -7,19 +7,19 @@
 function pendrell_header_buttons() {
   $buttons = apply_filters( 'pendrell_header_buttons', '' );
   if ( !empty( $buttons ) )
-    echo '<div class="buttons buttons-merge">' . $buttons . '</div>';
+    echo '<div class="buttons buttons-merge buttons-header">' . $buttons . '</div>';
 }
 add_action( 'pendrell_header_interface', 'pendrell_header_buttons', 10 );
 
 // Skip to content link
 function pendrell_header_skip_to_content( $buttons ) {
-  echo '<div class="buttons"><a href="#content" class="button skip-link" role="button" rel="nofollow">' . __( 'Skip to content', 'pendrell' ) . '</a></div>';
+  echo '<div class="buttons buttons-skip-link"><a href="#content" class="button skip-link" role="button" rel="nofollow">' . __( 'Skip to content', 'pendrell' ) . '</a></div>';
 }
 add_action( 'pendrell_header_interface', 'pendrell_header_skip_to_content', 15 ); // Because all these elements are floated right a higher number is displayed first
 
 // Responsive menu toggle
 function pendrell_header_responsive_menu() {
-  echo '<div class="buttons"><button id="menu-toggle" class="menu-toggle">' . pendrell_icon_text( 'menu-toggle', __( 'Menu', 'pendrell' ) ) . '</button></div>';
+  echo '<div class="buttons buttons-menu-toggle"><button id="menu-toggle" class="menu-toggle">' . pendrell_icon_text( 'menu-toggle', __( 'Menu', 'pendrell' ) ) . '</button></div>';
 }
 add_action( 'pendrell_header_interface', 'pendrell_header_responsive_menu', 5 );
 
@@ -51,6 +51,6 @@ add_action( 'pendrell_footer_navigation', 'pendrell_footer_menu', 5 );
 
 // Skip to top link
 function pendrell_footer_skip_to_top() {
-  echo '<div class="buttons"><a href="#page" class="button skip-top" rel="nofollow" role="button">' . pendrell_icon_text( 'top-link', __( 'Top', 'pendrell' ) ) . '</a></div>';
+  echo '<div class="buttons buttons-skip-top"><a href="#page" class="button skip-top" rel="nofollow" role="button">' . pendrell_icon_text( 'top-link', __( 'Top', 'pendrell' ) ) . '</a></div>';
 }
 add_action( 'pendrell_footer_navigation', 'pendrell_footer_skip_to_top', 20 );
